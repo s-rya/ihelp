bb62f039e83d266ed35bc70dbc76ebc6e76155eb
Surya Vaigundth M R <surya.vaigundth@in.ibm.com>
2017-05-23 22:21:00 +0530

    Exposed common-util

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
  * Created by iSmile on 5/10/2017.
  */
 const cloudant = require('./lib/cloudant');
+const commonUtil = require('./lib/common-util');
 
 module.exports = {
-    cloudant: cloudant
+    cloudant: cloudant,
+    commonUtil: commonUtil
 };
