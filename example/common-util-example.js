e21c183041a294d4e562be56a556e51e71bc0277
Surya Vaigundth M R <surya.vaigundth@in.ibm.com>
2017-06-14 16:47:47 +0530

    Added new method in common-util.js to generate HMAC encoded string

--- example/common-util-example.js
+++ example/common-util-example.js
@@ -5,3 +5,6 @@ const commonUtil = require('../lib/common-util');
 
 let date = commonUtil.getDate();
 console.log(date);
+
+let encodedString = commonUtil.createHMac('sha1','iSmile','myKey');
+console.log(encodedString);
