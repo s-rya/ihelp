9cb10cbb40c538abf79a987ad3fcffff0845f0da
Surya Vaigundth M R <surya.vaigundth@in.ibm.com>
2017-07-11 17:05:16 +0530

    Added method for Bulk Operation in Cloudant lib. Added comments.

--- example/cloudant-example.js
+++ example/cloudant-example.js
@@ -2,23 +2,39 @@
  * Created by iSmile on 5/10/2017.
  */
 const cloudant = require('../lib/cloudant');
+let docsArray = [
+    {
+        name: "iSmile"
+    },
+    {
+        name: "uSmile"
+    },
+    {
+        name: "weSmile"
+    }
+];
 
 cloudant.searchBySelector('user-history', {email: 'k@m.com'})
     .then(console.log)
     .catch(console.log);
 
-cloudant.createDoc('discovery-collection', {appName: 'list_of_application', appList: []})
+cloudant.createDatabase('test-db')
     .then(console.log)
     .catch(console.log);
 
-cloudant.getAllDocuments('users')
+cloudant.deleteDatabase('test-db')
     .then(console.log)
     .catch(console.log);
 
-cloudant.createDatabase('account-master-data')
+cloudant.createDoc('test-db', docsArray[1])
     .then(console.log)
     .catch(console.log);
 
-cloudant.deleteDatabase('account-master-data')
+cloudant.bulkOperation('test-db',docsArray)
     .then(console.log)
     .catch(console.log);
+
+cloudant.getAllDocuments('test-db')
+    .then(console.log)
+    .catch(console.log);
+
