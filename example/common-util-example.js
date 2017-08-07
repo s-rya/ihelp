69e415246f56865822e53b28ac6c6d3081554958
Surya Vaigundth M R <surya.vaigundth@in.ibm.com>
2017-08-07 16:23:19 +0530

    Added example for getDiffBetweenObj method

--- example/common-util-example.js
+++ example/common-util-example.js
@@ -8,3 +8,16 @@ console.log(date);
 
 let encodedString = commonUtil.createHMac('sha1','iSmile','myKey');
 console.log(encodedString);
+
+let a = {
+    "name" : "surya",
+    "age": "24"
+};
+
+let b = {
+    "name" : "ismile",
+    "age": "24"
+};
+
+let diff = commonUtil.getDiffBetweenObj(a, b);
+console.log(diff);
